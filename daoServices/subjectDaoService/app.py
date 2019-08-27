a2176bcacb5faac1b52c09edbe26c237492ec883
Alex <plast.r.alex@gmail.com>
2019-08-26 22:05:20 -0400

    Clean up patientDaoServices

--- daoServices/subjectDaoService/app.py
+++ daoServices/subjectDaoService/app.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask_sandboy import Sandboy
 from flask_sqlalchemy import SQLAlchemy
+import os
 # from models import Machine, Cloud,
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@0.0.0.0:5432/test'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_CONNECTION_STR']
 db = SQLAlchemy(app)
 
+########## Define Models #######################################################
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return self.name
+
+################################################################################
 
 db.init_app(app)
 with app.app_context():
